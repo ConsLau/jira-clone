87733aab2701c370dc2c0904b28ec3f87eb1b4bd
ConsLau <hiulau106@gmail.com>
2025-01-30 17:43:50 +1100

    customise components (button, input and select)

--- jira-clone/src/app/page.tsx
+++ jira-clone/src/app/page.tsx
@@ -1,12 +1,32 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
 import { TestComponent } from "@/features/test";
 
 export default function Home() {
   return (
     <div>
-      Hello World
-      <Button>Click me</Button>
-      <TestComponent></TestComponent>
+      <Input />
+      <Button variant="primary">
+        Primary
+      </Button>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      <Button variant="destructive">
+        Destructive
+      </Button>
+      <Button variant="ghost">
+        Ghost
+      </Button>
+      <Button variant="muted">
+        Muted
+      </Button>
+      <Button variant="outline">
+        Outline
+      </Button>
+      <Button variant="teritary">
+        Teritary
+      </Button>
     </div>
   );
 }
